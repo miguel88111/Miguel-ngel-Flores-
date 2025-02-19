b8ffa234dc930b38867155afc9c9a8ceec2a54b8
miguel88111 <miguelanguelflores56@gmail.com>
2025-02-19 10:50:52 -0300

    Update Codigo.js
    Traductor de lenguajes de programación

--- Codigo.js
+++ Codigo.js
@@ -1,1 +1,7 @@
 <script src="https://gist.github.com/tu-usuario/gist-id.js"></script>
+// Traductor simple de código
+function translateCode(languageFrom, languageTo, code) {
+    const translatedCode = `Traducción de ${languageFrom} a ${languageTo}: ${code}`;
+    console.log(`Traducción realizada: ${translatedCode}`);
+    return translatedCode;
+}
